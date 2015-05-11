ed507469b0dc639ac1b73e86f34c5bf735521138
root <jan@trick77.com>
2015-05-11 21:39:59 +0200

    Added support for BBC, iTV

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# -eq 0 ]
   then
-    docker build -f dockerflix.us -t trick77/dockerflix .
+    docker build --no-cache=true -f dockerflix.us -t trick77/dockerflix .
   else
-    docker build -f dockerflix.$1 -t trick77/dockerflix . 
+    docker build --no-cache=true -f dockerflix.$1 -t trick77/dockerflix .
 fi
